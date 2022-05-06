0efd03ad8c6b33197c91fadbfdfa3d455b3a456d
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2022-05-06 09:41:39 -0700

    SQL-865: Adding logging parameters to connection dialogue (#9)

--- connector/connectionProperties.js
+++ connector/connectionProperties.js
@@ -5,5 +5,13 @@
     props["database"] = attr[connectionHelper.attributeDatabase];
     props["dialect"] = "mongosql";
 
+    if (attr["v-log-directory"] == null) {
+        props["LogDir"] = "console";
+    } else {
+        props["LogDir"] = attr["v-log-directory"];
+    }
+
+    props["LogLevel"] = attr["v-loglevel"];
+
     return props;
 })
