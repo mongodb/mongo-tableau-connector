58eb57a4cfddf79069ef835dcbc6eef5b94476ca
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2022-05-18 10:32:35 -0700

    Update ClientInfo to use '+' as delimiter

--- connector/connectionProperties.js
+++ connector/connectionProperties.js
@@ -4,7 +4,7 @@
     props["password"] = attr[connectionHelper.attributePassword];
     props["database"] = attr[connectionHelper.attributeDatabase];
     props["dialect"] = "mongosql";
-    props["ClientInfo"] = "tableau-connector|0.0.0";
+    props["ClientInfo"] = "tableau-connector+0.0.0";
 
     props["LogDir"] = "console";
     if (attr["v-log-directory"] != null && attr["v-log-directory"] !== "") {
