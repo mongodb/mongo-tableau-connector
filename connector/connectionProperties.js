37dcc62fd7bbd4efce3e726554ef02eee35d4460
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2024-05-31 13:36:47 -0700

    SQL-2082: Make UID and PWD optional in connection dialog (#58)
    * SQL-2082: Make UID and PWD optional in connection dialog
    
    * SQL-2082: Update label for auth-none

--- connector/connectionProperties.js
+++ connector/connectionProperties.js
@@ -1,7 +1,12 @@
 (function propertiesbuilder(attr) {
     var props = {};
-    props["user"] = attr[connectionHelper.attributeUsername];
-    props["password"] = attr[connectionHelper.attributePassword];
+
+    var auth = attr[connectionHelper.attributeAuthentication];
+    if (auth === "auth-user-pass") {
+        props["user"] = attr[connectionHelper.attributeUsername];
+        props["password"] = attr[connectionHelper.attributePassword];
+    }
+
     props["database"] = attr[connectionHelper.attributeDatabase];
     props["dialect"] = "mongosql";
     props["clientInfo"] = "tableau-connector+0.0.0";
